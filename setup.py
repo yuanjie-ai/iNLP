06cb56143fdc53a35f08bac72f5f97d38d7977e2
尐 <313303303@qq.com>
2018-06-04 16:58:37 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md") as f:
 
 setup(
     name='iNLP',
-    version='1.0.1',
+    version='1.0.0',
     url='https://github.com/Jie-Yuan/iNLP',
     keywords=["iNLP", "313303303@qq.com"],
     
