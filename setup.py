497e67d92372c2e6b4bbb7d53c5a8fa63ed0e59d
尐 <313303303@qq.com>
2018-06-04 17:23:33 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     url='https://github.com/Jie-Yuan/iNLP',
     keywords=["iNLP", "313303303@qq.com"],
     
-    description=('Spark human utils'),
+    description=('NLP human utils'),
     packages=find_packages(),
     long_description=long_description,
     long_description_content_type="text/markdown",
