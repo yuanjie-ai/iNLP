886fb3f4a6e8c700ee16bbb7b1e08110fd09898e
尐 <313303303@qq.com>
2018-06-04 16:55:10 +0800

    Create __init__.py

---
+++ inlp/convert/char/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+FULL2HALF = {i + 0xFEE0: i for i in range(0x21, 0x7F)}
+FULL2HALF[0x3000] = 0x20
+HALF2FULL = {j: i for i, j in FULL2HALF.items()}
+
+
+def halfen(s):
+    '''
+    Convert full-width characters to ASCII counterpart
+    '''
+    return str(s).translate(FULL2HALF)
+
+
+def fullen(s):
+    '''
+    Convert all ASCII characters to the full-width counterpart.
+    '''
+    return str(s).translate(HALF2FULL)
