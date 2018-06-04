d904b5cf2b8e27a521aa92abae15cf9905168ade
尐 <313303303@qq.com>
2018-06-04 16:14:45 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name='iNLP',
+    version='1.0.1',
+    url='https://github.com/Jie-Yuan/iNLP',
+    keywords=["iNLP", "313303303@qq.com"],
+    
+    description=('Spark human utils'),
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='JieYuan',
+    author_email='313303303@qq.com',
+    maintainer='JieYuan',
+    maintainer_email='313303303@qq.com',
+    license='MIT',
+    include_package_data=True,
+    platforms=["all"],
+    python_requires='>=2.7, <4',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries'
+    ]
+)
