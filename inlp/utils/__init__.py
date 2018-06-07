ad5b0cd187da696617d13ad4272cd25eb195ca79
尐 <313303303@qq.com>
2018-06-07 15:08:44 +0800

    Update __init__.py

--- inlp/utils/__init__.py
+++ inlp/utils/__init__.py
@@ -7,14 +7,7 @@ __mtime__ = '2018/6/5'
 import os
 
 
-def _get_module_path(path, f):
-    """
-
-    :param path:
-    :param f: __file__
-    :return:
-    """
-    return os.path.normpath(os.path.join(os.getcwd(), os.path.dirname(f), path))
+_get_module_path = lambda path, file: os.path.normpath(os.path.join(os.getcwd(), os.path.dirname(f), path))
 
 
 _cilin_path = _get_module_path('../data/cilin2.txt', __file__)
